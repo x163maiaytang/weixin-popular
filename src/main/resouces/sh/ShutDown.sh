b306cca243253a31097f5731c55da1231810a772
x163maiyatng <x163maiyatang@163.com>
2019-06-27 09:11:19 +0800

    update

--- src/main/resouces/sh/ShutDown.sh
+++ src/main/resouces/sh/ShutDown.sh
@@ -1,41 +1,12 @@
 #!/bin/bash
-P=8080
+P=8099
 PORT=$(netstat -ntulp | grep $P)
 
-FLAG=""
-PIDD=""
-RETT=""
-# 如果PORT不为空说明有程序占用该端口号
-if [[ -n $PORT ]]; then
+FLAG="OK"
 
+PIDD=$(netstat -nlp | grep -w $P | sed -r 's#.* (.*)/.*#\1#')
 
-  	PIDD=$(netstat -nlp | grep -w $P | sed -r 's#.* (.*)/.*#\1#')
-        echo $PIDD
-
-
-        echo "The server is open, ready to close"
-        RESULT=""
-	
-        while [[ $RESULT == "" || $RETT == "" ]]; do
-                echo "Closing down ..."
-                # 优雅关闭springboot项目
-                RESULT=$(curl -X POST 127.0.0.1:$P/actuator/shutdown)
-
-
-		RETT=$(echo $RESULT | grep "bye")
-                sleep 2
-        done
-
-   FLAG="OK"
-fi
-
-     PORT=$(netstat -ntulp | grep $P)
-while [[ -n $PORT ]]; do
-     echo "Closing down ..."
-     sleep 2
-     PORT=$(netstat -ntulp | grep $P)
-done
-
+echo $PIDD
 
 if [[ $FLAG == "OK" ]]; then
 
--- src/main/resouces/sh/StartUp.sh
+++ src/main/resouces/sh/StartUp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 env=$1
 if [ ! -n "$1" ];then
-env=prod 
+env=default 
 fi
-nohup java -jar -server -Xms256m -Xmx1g -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=256m -XX:NewRatio=3 -XX:+UseConcMarkSweepGC -XX:ErrorFile=./hs_errp.log -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./heap_dump_errp.hprof -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xloggc:./gc.log -Dloader.path=.,3rd-lib weixin-popular-2.8.24-classes.jar --spring.profiles.active=$env
+nohup java -jar -server -Xms256m -Xmx1g -XX:MetaspaceSize=128m -XX:MaxMetaspaceSize=256m -XX:NewRatio=3 -XX:+UseConcMarkSweepGC -XX:ErrorFile=./hs_errp.log -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=./heap_dump_errp.hprof -XX:+PrintGCDetails -XX:+PrintGCTimeStamps -Xloggc:./gc.log -Dloader.path=.,3rd-lib weixin-popular-2.8.24-classes.jar --spring.profiles.active=$env&
 
-#tail -f nohup.out
+tail -f nohup.out
